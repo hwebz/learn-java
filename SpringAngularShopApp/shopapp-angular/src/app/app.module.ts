80c8a0c22caefc4940d2513aded5608e9b65dab5
Ha Manh Do <hadm.haui@gmail.com>
2024-04-18 03:11:52 +0700

    Design Products + Detail pages

--- SpringAngularShopApp/shopapp-angular/src/app/app.module.ts
+++ SpringAngularShopApp/shopapp-angular/src/app/app.module.ts
@@ -3,16 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    ProductDetailComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [HomeComponent]
+  bootstrap: [ProductDetailComponent]
 })
 export class AppModule { }
