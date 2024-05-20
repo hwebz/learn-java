68286e748ef74e5e9a2bb8961106ad32b9c15162
Ha Manh Do <hadm.haui@gmail.com>
2024-05-20 16:06:22 +0700

    Showing role dropdown on login page

--- SpringAngularShopApp/shopapp-angular/src/app/components/login/login.component.ts
+++ SpringAngularShopApp/shopapp-angular/src/app/components/login/login.component.ts
@@ -1,27 +1,52 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 import { UserService } from '../../services/user.service';
 import { LoginDTO } from '../../dtos/user/login.dto';
 import LoginResponse from '../../responses/user/login.response';
 import TokenService from '../../services/token.service';
+import RoleResponse from '../../responses/role.response';
+import RoleService from '../../services/role.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
   @ViewChild("loginForm") loginForm!: NgForm;
   phoneNumber: string = '';
   password: string = '';
   rememberMe: boolean = false;
+  selectedRole?: RoleResponse;
+  roles: RoleResponse[] = [];
 
   constructor(
     private router: Router,
     private userService: UserService,
-    private tokenService: TokenService
+    private tokenService: TokenService,
+    private roleService: RoleService
   ) {}
+  
+  ngOnInit(): void {
+    this.fetchRoles();
+  }
+
+  private fetchRoles() {
+    this.roleService.getRoles()
+    .subscribe({
+      next: (response: RoleResponse[]) => {
+        this.roles = response;
+        this.selectedRole = response?.[1]
+      },
+      complete: () => {
+        console.log('fetch roles completed')
+      },
+      error: (e: any) => {
+        alert(e.error)
+      }
+    });
+  }
 
   async login() {
     const loginData = new LoginDTO({
