4c57fa4c7f567e1c2c125e0a3dd4738a4bdc79e2
Ha Manh Do <hadm.haui@gmail.com>
2025-03-11 11:09:28 +0700

    Implement API gateway

--- spring-boot-microservices-k8s-angular/inventory-service/src/test/java/com/hwebz/microservices/inventory/InventoryServiceApplicationTests.java
+++ spring-boot-microservices-k8s-angular/inventory-service/src/test/java/com/hwebz/microservices/inventory/InventoryServiceApplicationTests.java
@@ -1,15 +1,54 @@
 package com.hwebz.microservices.inventory;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.containers.MySQLContainer;
+import io.restassured.RestAssured;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
-@Import(TestcontainersConfiguration.class)
-@SpringBootTest
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class InventoryServiceApplicationTests {
 
+	@ServiceConnection
+	static MySQLContainer mySQLContainer = new MySQLContainer("mysql:8.3.0");
+	@LocalServerPort
+	private Integer port;
+
+	@BeforeEach
+	void setup() {
+		RestAssured.baseURI = "http://localhost";
+		RestAssured.port = port;
+	}
+
+	static {
+		mySQLContainer.start();
+	}
+
 	@Test
-	void contextLoads() {
+	void shouldReadInventory() {
+		var response = RestAssured.given()
+				.when()
+				.get("/api/v1/inventory?skuCode=iphone_15&quantity=50")
+				.then()
+				.log().all()
+				.statusCode(200)
+				.extract().response().as(Boolean.class);
+		assertTrue(response);
+
+		var negativeResponse = RestAssured.given()
+				.when()
+				.get("/api/v1/inventory?skuCode=iphone_15&quantity=500")
+				.then()
+				.log().all()
+				.statusCode(200)
+				.extract().response().as(Boolean.class);
+		assertFalse(negativeResponse);
 	}
 
 }
--- spring-boot-microservices-k8s-angular/inventory-service/src/test/java/com/hwebz/microservices/inventory/TestInventoryServiceApplication.java
+++ spring-boot-microservices-k8s-angular/inventory-service/src/test/java/com/hwebz/microservices/inventory/TestInventoryServiceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 public class TestInventoryServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.from(InventoryServiceApplication::main).with(TestcontainersConfiguration.class).run(args);
+		SpringApplication.from(InventoryServiceApplication::main).run(args);
 	}
 
 }
